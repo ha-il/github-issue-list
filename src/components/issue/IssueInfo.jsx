fe6ebd7c447bd03b53205cc7f6196947aed6181d
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-08-30 15:01:28 +0900

    refactor: 이슈 상세 정보 중복 해소를 위해 IssueInfo 컴포넌트 추가

---
+++ src/components/issue/IssueInfo.jsx
@@ -0,0 +1,38 @@
+import { styled } from 'styled-components';
+
+import { formatDate } from '../../utils';
+
+function IssueInfo({ issue }) {
+  const {
+    number,
+    title,
+    updated_at,
+    comments,
+    user: { login },
+  } = issue;
+
+  return (
+    <IssueInfoStyle>
+      <div>
+        <h2>
+          #{number} {title}
+        </h2>
+        <span>
+          작성자: {login}, 작성일: {formatDate(updated_at)}
+        </span>
+      </div>
+      <div>코멘트: {comments}</div>
+    </IssueInfoStyle>
+  );
+}
+
+const IssueInfoStyle = styled.div`
+  width: 100%;
+  border-bottom: 1px solid gray;
+  padding-bottom: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export default IssueInfo;
