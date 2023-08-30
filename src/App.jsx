54b60fd5e412353736e5e497585162041e8e6a3f
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-08-30 14:58:55 +0900

    fix: 이슈 상세페이지 동적 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import './App.css';
 import Layout from './components/layout/Layout';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
+import IssueDetailPage from './pages/IssueDetailPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
     errorElement: <ErrorPage />,
-    children: [{ path: '/', element: <HomePage /> }],
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: ':issueNumber', element: <IssueDetailPage /> },
+    ],
   },
 ]);
 
