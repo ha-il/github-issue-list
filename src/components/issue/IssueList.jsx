a029695551d7c641ab95be3dddd40ec77b787451
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-09-01 21:01:22 +0900

    fix: throttle 함수 추가하여 무한 스크롤 렌더링 버그 수정

--- src/components/issue/IssueList.jsx
+++ src/components/issue/IssueList.jsx
@@ -1,47 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { styled } from 'styled-components';
 
 import { getIssues } from '../../apis';
-import { sliceArray } from '../../utils';
+import { sliceArray, throttle } from '../../utils';
 import Message from '../common/Message';
 
 import IssueGroup from './IssueGroup';
 
 function IssueList() {
-  const [issues, setIssues] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [issues, setIssues] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const fetchIssues = async page => {
-    try {
-      setErrorMessage('');
-      setIsLoading(true);
-      const { data } = await getIssues(page);
-      setIssues(prev => [...prev, ...data]);
-      setPageNumber(prev => prev + 1);
-    } catch (error) {
-      setErrorMessage(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const fetchIssues = useCallback(
+    async page => {
+      try {
+        const { data } = await getIssues(page);
+        setErrorMessage('');
+        setIsLoading(true);
+        setIssues(prev => [...prev, ...data]);
+        setPageNumber(pageNumber + 1);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [pageNumber],
+  );
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      if ((window.innerHeight + scrollTop >= offsetHeight) & (isLoading === false)) {
+        fetchIssues(pageNumber);
+      }
+    });
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [pageNumber]);
 
   useEffect(() => {
     fetchIssues(pageNumber);
   }, []);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, offsetHeight } = document.documentElement;
-      if (window.innerHeight + scrollTop >= offsetHeight) {
-        fetchIssues(pageNumber);
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [pageNumber]);
 
   const listNumPerAdImage = 4;
   const sliceSize = listNumPerAdImage;
--- src/utils/index.js
+++ src/utils/index.js
@@ -16,3 +16,25 @@ export const formatDate = dateString => {
   const formattedDate = `${year}년 ${month}월 ${day}일`;
   return formattedDate;
 };
+
+export const throttle = (handler, timeout = 300) => {
+  let invokedTime;
+  let timer;
+  return function (...args) {
+    if (!invokedTime) {
+      handler.apply(this, args);
+      invokedTime = Date.now();
+    } else {
+      clearTimeout(timer);
+      timer = window.setTimeout(
+        () => {
+          if (Date.now() - invokedTime >= timeout) {
+            handler.apply(this, args);
+            invokedTime = Date.now();
+          }
+        },
+        Math.max(timeout - (Date.now() - invokedTime), 0),
+      );
+    }
+  };
+};
